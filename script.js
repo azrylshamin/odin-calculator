78c9ccbc3bdb9cf278405bdf26b36ee31ba0ec99
Azryl Shamin <azryl.career@gmail.com>
2025-05-07 00:54:01 +0800

    Implement function to access all the common functions in console

--- script.js
+++ script.js
@@ -1,3 +1,26 @@
+let firstOperand;
+let operator;
+let secondOperand;
+
+function operate(firstOperand, secondOperand, operator) {
+    switch (operator) {
+        case '+':
+            console.log(add(firstOperand, secondOperand));
+            break;
+        case '-':
+            console.log(subtract(firstOperand, secondOperand));
+            break;
+        case '*':
+            console.log(multiply(firstOperand, secondOperand));
+            break;
+        case '/':
+            console.log(divide(firstOperand, secondOperand));
+            break;
+        default:
+            console.log("Invalid operator!")
+    }
+}
+
 function add(a, b) {
     return a + b;
 }
